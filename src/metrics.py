Fixed learning rate and RMSE calculation.

src/metrics.py
@@ -9,33 +9,33 @@ def to_numpy(arr):
     return arr
 
 
-def rmse(y_true, y_pred, **kwargs):
+def rmse(y_true, y_sim, **kwargs):
     """ Root Mean Square Error """
-    y_true, y_pred = map(to_numpy, (y_true, y_pred))
-    return mean_squared_error(y_true, y_pred, squared=False, **kwargs)
+    y_true, y_sim = map(to_numpy, (y_true, y_sim))
+    return mean_squared_error(y_true, y_sim, squared=False, **kwargs)
 
 
-def pi(y_pred, interval):
+def pi(y_sim, interval):
     """ Predictive Interval """
     # TODO: may need to be exact
     assert 0. <= interval <= 1.
     lower_bound = (1. - interval) / 2
     upper_bound = 1. - lower_bound
-    lower_quantile, upper_quantile = np.quantile(y_pred, [lower_bound, upper_bound], axis=0)
+    lower_quantile, upper_quantile = np.quantile(y_sim, [lower_bound, upper_bound], axis=0)
     return lower_quantile, upper_quantile
 
 
-def picp(y_true, y_pred, interval=.95):
+def picp(y_true, y_sim, interval=.95):
     r""" Prediction Interval Coverage Probability is $1/n \sum 1_{y_{L_i} \leq y_i \leq y_{U_i}$ where
     $P(y_{L_i} \leq y_i \leq y_{U_i}) \geq \gamma$. """
-    y_true, y_pred = map(to_numpy, (y_true, y_pred))
-    lower_quantile, upper_quantile = pi(y_pred, interval)
+    y_true, y_sim = map(to_numpy, (y_true, y_sim))
+    lower_quantile, upper_quantile = pi(y_sim, interval)
     coverage = np.bitwise_and(lower_quantile <= y_true, y_true <= upper_quantile)
     return np.mean(coverage)
 
 
-def mpiw(y_pred, interval=.95):
+def mpiw(y_sim, interval=.95):
     r""" Mean Prediction Interval Width is $1/n \sum y_{U_i} - y_{L_i}$ """
-    y_pred = to_numpy(y_pred)
-    lower_quantile, upper_quantile = pi(y_pred, interval)
+    y_sim = to_numpy(y_sim)
+    lower_quantile, upper_quantile = pi(y_sim, interval)
     return np.mean(upper_quantile - lower_quantile)

src/mnf/multiplicative_normalizing_flow.py
@@ -26,7 +26,7 @@ class MNFFeedForwardNetwork(nn.Module):
         return x
 
     def log_likelihood(self, x, y):
-        return torch.distributions.Normal(self(x).reshape(y.shape), .1).log_prob(y)
+        return torch.distributions.Normal(self(x).reshape(y.shape), .01).log_prob(y)
 
     def loss(self, x, y):
         return -(self.log_likelihood(x, y) + sum(layer.kl_divergence() for layer in self.layers)).mean()